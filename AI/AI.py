21c3711284106efab7a2735308b42080341d8ddd
Yapock <yapock123@gmail.com>
2023-06-01 09:24:28 +0200

    ajout d'un système de sauvegarde à l'IA

--- AI/AI.py
+++ AI/AI.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+import emoji
 from EdgeGPT import Chatbot, ConversationStyle
 
 async def generate_IA(prompt):
@@ -22,6 +23,29 @@ async def generate_IA(prompt):
 
 
     await bot.close()
+
+    sauv = int(input("Voulez-vous sauvegardé la réponse :\n1) Oui\n2) Non\nVotre réponse : "))
+
+    if sauv == 1 :
+        titre = input("Donné un nom au Sujet : ")
+
+        sauvegarde(titre, bot_response)
+    else :
+        print("Merci d'avoir utilisé Jake")
+
+def supprimer_emoji(chaine):
+    chaine_texte = emoji.demojize(chaine)
+    
+    return chaine_texte
+
+
+def sauvegarde(Titre, Jake):
+    Jake = supprimer_emoji(Jake)
+
+    with open(f"./Sauvegarde/{Titre}.txt", "w") as f :
+        f.write(Jake)
+
+    print(f"Votre fichier à été sauvegardés: {Titre}.txt")
 
 
 text = input("Prompt : ")
